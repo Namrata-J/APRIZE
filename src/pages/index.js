b6e170c1f2f1200074828e21095e544434592590
Namrata-J <namratajain29001@gmail.com>
2022-03-22 12:57:00 +0530

    common folder importing at and exporting pages from a common place

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import { HomePage } from "./Home/Home";
+import { ProductListingPage } from "./Product-listing/Product-listing";
+import { WishlistPage } from "./Wishlist/Wishlist";
+import { CartPage } from "./Cart/Cart";
+import { LogInPage } from "./Authentication/LogIn/LogIn";
+import { LogOutPage } from "./Authentication/LogOut/LogOut";
+import { SignUpPage } from "./Authentication/SignUp/SignUp";
+
+export { HomePage, ProductListingPage, WishlistPage, CartPage, LogInPage, LogOutPage, SignUpPage };
