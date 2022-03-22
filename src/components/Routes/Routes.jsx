e0e3bcccd127a63c545c66002fa888e0b42f928e
Namrata-J <namratajain29001@gmail.com>
2022-03-22 12:17:11 +0530

    feat: made a routes component

---
+++ src/components/Routes/Routes.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { HomePage, ProductListingPage, WishlistPage, CartPage, LogInPage, LogOutPage, SignUpPage } from "../../pages/index";
+
+const RoutesComponent = () => {
+    return (
+        <Routes>
+            <Route path ="/" element= {<HomePage />} />
+            <Route path ="/Productlisting" element= {<ProductListingPage />} />
+            <Route path ="/Wishlist" element= {<WishlistPage />} />
+            <Route path ="/Cart" element= {<CartPage />} />
+            <Route path ="/LogIn" element= {<LogInPage />} />
+            <Route path ="/LogOut" element= {<LogOutPage />} />
+            <Route path ="/SignUp" element= {<SignUpPage />} />
+        </Routes>
+    );
+}
+
+export { RoutesComponent };
