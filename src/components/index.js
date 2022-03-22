b6f5301c988b808e246d245f9fc2e01adff0efbd
Namrata-J <namratajain29001@gmail.com>
2022-03-22 12:35:21 +0530

    added one common file for all the components which exports all components from a single path

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import { Header } from "./Header/Header";
+import { RoutesComponent } from "./Routes/Routes";
+import { CategoryType } from "./Category-type/CategoryType";
+import { ShopNowImgSection } from "./ShopNowImgSection/ShopNowImgSection";
+import { NewArrivalsSection } from "./New-Arrivals/NewArrivalsSection";
+import { DiscountedProducts } from "./Discounted-Products/DiscountedProducts";
+import { ProductsOnSale } from "./Products-on-sale/ProductsOnSale";
+import { Footer } from "./Footer/Footer";
+
+export { Header, RoutesComponent, CategoryType, ShopNowImgSection, NewArrivalsSection, DiscountedProducts, ProductsOnSale, Footer };
