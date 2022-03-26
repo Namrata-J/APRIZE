e7c1cd6b6cd4cefd3c1eaa453108dcc102d1692f
Namrata-J <namratajain29001@gmail.com>
2022-03-26 11:16:51 +0530

    feat: made product component where listing of all the products happens

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,24 @@
+import "./product.css";
+import { ProductCard } from "../Product-card/ProductCard";
+import { useProductsDetail } from "../../contexts/productsDetail-context";
+import { FloatingBtn } from "../Floating-Btn/FloatingBtn";
+
+const Product = () => {
+
+    const { products } = useProductsDetail();
+
+    return (
+        <div className="ap_productListing-page-products-section">
+            <div className="ap_product-listing">
+                <h4 className="ap_showProducts-count-heading">
+                    <FloatingBtn />
+                    Showing Products
+                    <span className="ap_all-product-listing fw-1">(Showing {products.length} products)</span>
+                </h4>
+                <ProductCard />
+            </div>
+        </div>
+    );
+}
+
+export { Product };
