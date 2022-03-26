2ca2460aff5d81b672905f09b3f12dfa8d5365b5
Namrata-J <namratajain29001@gmail.com>
2022-03-26 11:20:49 +0530

    update: imported all the new components at one place

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,10 @@ import { NewArrivalsSection } from "./New-Arrivals/NewArrivalsSection";
 import { DiscountedProducts } from "./Discounted-Products/DiscountedProducts";
 import { ProductsOnSale } from "./Products-on-sale/ProductsOnSale";
 import { Footer } from "./Footer/Footer";
+import { SideBar } from "./Side-bar/SideBar";
+import { Product } from "./Product/Product";
+import { ProductCard } from "./Product-card/ProductCard";
+import { FloatingBtn } from "./Floating-Btn/FloatingBtn";
+import { FilterableForm } from "./Filterable-Form/FilterableForm";
 
-export { Header, RoutesComponent, CategoryType, ShopNowImgSection, NewArrivalsSection, DiscountedProducts, ProductsOnSale, Footer };
+export { Header, RoutesComponent, CategoryType, ShopNowImgSection, NewArrivalsSection, DiscountedProducts, ProductsOnSale, Footer, SideBar, Product, ProductCard, FloatingBtn, FilterableForm };
