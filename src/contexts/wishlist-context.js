970a036e1051528d2d5be3894f3741dfd6737aa3
Namrata-J <namratajain29001@gmail.com>
2022-03-30 02:59:33 +0530

    feat: context for wishlist functionality

---
+++ src/contexts/wishlist-context.js
@@ -0,0 +1,35 @@
+import React, { useReducer, useContext, createContext } from "react";
+
+const wishlistContext = createContext({ state: [] });
+
+const WishlistProvider = ({ children }) => {
+
+    const getLikeButtonStyle = (product) => {
+        if(stateOfWishlist.includes(product)){
+            return "var(--light-red)"
+        }else
+            return "var(--black)"
+    }
+
+    const wishlistReducer = (stateOfWishlist, action) => {
+        switch(action.type){
+            case "ADD_TO_WISHLIST":
+                if( !(stateOfWishlist.includes(action.payload)) ){
+                    return  [ ...stateOfWishlist, action.payload ]
+                }else
+                    return stateOfWishlist.filter(item => item._id!==action.payload._id)
+            case "REMOVE_FROM_WISHLIST":
+                    return stateOfWishlist.filter(item => item._id!==action.payload._id)
+        }
+    }
+
+    const [ stateOfWishlist , dipatchOfWishlist ] = useReducer( wishlistReducer, [] );
+
+    return <wishlistContext.Provider value={{ stateOfWishlist, dipatchOfWishlist, getLikeButtonStyle }}>
+        { children }
+    </wishlistContext.Provider>
+}
+
+const useWishlist = () => useContext(wishlistContext);
+
+export { useWishlist, WishlistProvider };
