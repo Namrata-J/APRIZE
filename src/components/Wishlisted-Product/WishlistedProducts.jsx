53a85140ea8aaf87d0fcc91297121ac24e8f3d67
Namrata-J <namratajain29001@gmail.com>
2022-03-30 03:00:11 +0530

    feat: added remove functionality to cards

--- src/components/Wishlisted-Product/WishlistedProducts.jsx
+++ src/components/Wishlisted-Product/WishlistedProducts.jsx
@@ -1,90 +1,60 @@
 import "./wishlistedProduct.css";
 import { FaRegHeart } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import { getProductClass, getOriginalPrice } from "../../utils/productutilFuncs";
+import { useWishlist } from "../../contexts/wishlist-context";
 
 const WishlistProduct = () => {
+
+    const { stateOfWishlist:wishlistedProductsToBeShown, dipatchOfWishlist } = useWishlist();
+
     return (
         <div className="ap_all-wishlisted-products">
             <h3 className="ap_wislist-main-section-heading a-tc">My wishList</h3>
-            
+
             <div className="ap_wislisted-products-container">
+                {
+                    wishlistedProductsToBeShown.length === 0 ?
+                        <div className="ap_empty-wishlist-img-container">
+                            <img className="ap_empty-wishlist-img" src="https://media.istockphoto.com/vectors/empty-shopping-bag-icon-online-business-vector-icon-template-vector-id861576608?k=20&m=861576608&s=612x612&w=0&h=UgHaPYlYrsPTO6BKKTzizGQqFgqEnn7eYK9EOA16uDs=" />
+                            <button className="et_p-simple-btn action-color btn">
+                                <Link to="/Productlisting" className="ap_shop-Now-link">Add Now</Link>
+                            </button>
+                        </div> : wishlistedProductsToBeShown.map((wishlistedProduct, index) => {
+                            return (
+                                <div className={getProductClass(wishlistedProduct)} key={index}>
+                                    <div className="badge-type1 fw-4">New</div>
+                                    {wishlistedProduct.isNewArrival ? <div className="badge-type1 fw-4">New</div> : ""}
+                                    {!wishlistedProduct.isInStock ? <div className="badge-type3">Stock Out</div> : ""}
+                                    <i className="ap_wishlisted-product-icon"><FaRegHeart /></i>
 
-                <div className="card-w-badge-type1 card-w-badge b-rad1">
-                    <div className="badge-type1 fw-4">New</div>
-                    <i className="ap_wishlisted-product-icon"><FaRegHeart /></i>
-
-                    <div className="card-w-badge-subcontainer1">
-                    </div>
-                    <div className="card-w-badge-subcontainer2">
-                        <div className="product-description">
-                            <div className="card-title fw-4 a-tl">Nike</div>
-                            <div className="card-subtitle a-tl">Comfortable shoes for men</div>
-                        </div>
-                        <div className="card-product-price fw-3">
-                            <div className="ap_card-pp a-tl">
-                                <span className="p-mrp fw-1 a-tl">1000</span>
-                                <span className="p-discounted-price a-tl">₹900</span>
-                            </div>
-                            <div className="p-dicount a-tl">90% off</div>
-                        </div>
-                    </div>
-                    <div className="card-w-badge-subcontainer3">
-                        <button className="et_p-simple-btn action-color btn">Add To Cart</button>
-                        <button className="et_so-btn action-color btn">Remove</button>
-                    </div>
-                </div>
-
-                <div className="card-w-badge-type2 card-w-badge b-rad1">
-                    <i className="ap_wishlisted-product-icon"><FaRegHeart /></i>
-
-                    <div className="card-w-badge-subcontainer1">
-                    </div>
-                    <div className="card-w-badge-subcontainer2">
-                        <div className="product-description">
-                            <div className="card-title fw-4 a-tl">Nike</div>
-                            <div className="card-subtitle a-tl">Comfortable shoes for men</div>
-                        </div>
-                        <div className="card-product-price fw-3">
-                            <div className="ap_card-pp a-tl">
-                                <span className="p-mrp fw-1 a-tl">1000</span>
-                                <span className="p-discounted-price a-tl">₹900</span>
-                            </div>
-                            <div className="p-dicount a-tl">90% off</div>
-                        </div>
-                    </div>
-                    <div className="card-w-badge-subcontainer3">
-                        <button className="et_p-simple-btn action-color btn">Add To Cart</button>
-                        <button className="et_so-btn action-color btn">Remove</button>
-                    </div>
-                </div>
-
-                <div className="card-w-badge-type3 card-w-badge b-rad1">
-                    <div className="badge-type3">Stock Out</div>
-                    <i className="ap_wishlisted-product-icon"><FaRegHeart /></i>
-
-                    <div className="card-w-badge-subcontainer1">
-                    </div>
-                    <div className="card-w-badge-subcontainer2">
-                        <div className="product-description">
-                            <div className="card-title fw-4 a-tl">Nike</div>
-                            <div className="card-subtitle a-tl">Comfortable shoes for men</div>
-                        </div>
-                        <div className="card-product-price fw-3">
-                            <div className="ap_card-pp a-tl">
-                                <span className="p-mrp fw-1 a-tl">1000</span>
-                                <span className="p-discounted-price a-tl">₹900</span>
-                            </div>
-                            <div className="p-dicount a-tl">90% off</div>
-                        </div>
-                    </div>
-                    <div className="card-w-badge-subcontainer3">
-                        <button className="et_p-simple-btn action-color btn" disabled>Add To Cart</button>
-                        <button className="et_so-btn action-color btn" disabled>Remove</button>
-                    </div>
-                </div>
-
+                                    <div className="card-w-badge-subcontainer1" style= {{ backgroundImage: `url(${ wishlistedProduct.img })`}}>
+                                    </div>
+                                    <div className="card-w-badge-subcontainer2">
+                                        <div className="product-description">
+                                            <div className="card-title fw-4 a-tl">{ wishlistedProduct.brandName }</div>
+                                            <div className="card-subtitle a-tl">{ wishlistedProduct.description }</div>
+                                        </div>
+                                        <div className="card-product-price fw-3">
+                                            <div className="ap_card-pp a-tl">
+                                                <span className="p-mrp fw-1 a-tl">{ getOriginalPrice(wishlistedProduct) }</span>
+                                                <span className="p-discounted-price a-tl">{`₹${ wishlistedProduct.discountedPrice }`}</span>
+                                            </div>
+                                            <div className="p-dicount a-tl">{ wishlistedProduct.discount==="" ? "" : `${ wishlistedProduct.discount }%` }</div>
+                                        </div>
+                                    </div>
+                                    <div className="card-w-badge-subcontainer3">
+                                        <button className="et_p-simple-btn action-color btn" disabled = { !wishlistedProduct.isInStock } >Add To Cart</button>
+                                        <button className="et_so-btn action-color btn" onClick={() => dipatchOfWishlist({ type: "REMOVE_FROM_WISHLIST", payload: wishlistedProduct })}>Remove</button>
+                                    </div>
+                                </div>
+                            );
+                        })
+                }
             </div>
         </div>
     );
 }
 
 export { WishlistProduct };
+
