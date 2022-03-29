ef5caabca1352be5cd60b14fb9f54cc4e8f8190d
Namrata-J <namratajain29001@gmail.com>
2022-03-29 13:26:30 +0530

    feat: added ui for cart page

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./cart.css";
+import { Header, ProductsInCart, Footer } from "../../components";
 
 const CartPage = () => {
     return (
-        <div>
-
+        <div className="ap_cart-page">
+            <div className="ap_cart-header-section">
+                <Header />
+            </div>
+            <div className="ap_cart-main-section">
+                <ProductsInCart />
+                <Footer />
+            </div>
         </div>
     );
 }
