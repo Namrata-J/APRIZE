e60da486243c788363370038d8646361dd18c06e
Namrata-J <namratajain29001@gmail.com>
2022-03-30 15:43:03 +0530

    feat: added increase and decrease quantity functionality

--- src/contexts/cart-context.js
+++ src/contexts/cart-context.js
@@ -1,4 +1,5 @@
 import React, {useReducer, useContext, createContext } from "react";
+import { ProductsInCart } from "../components/Products-In-Cart/ProductsInCart";
 
 const cartContext = createContext({ state: [] });
 
@@ -7,12 +8,16 @@ const CartProvider = ({ children }) => {
     const cartReducer = (stateOfCart, action) => {
         switch(action.type){
             case "ADD_TO_CART":
-                if( !(stateOfCart.includes(action.payload)) ){
-                     return [ ...stateOfCart, action.payload ]
+                if( !(stateOfCart.some(item => item._id === action.payload._id)) ){
+                     return [ ...stateOfCart, { ...action.payload, qt: 1 } ]
                 }else
-                     return [ ...stateOfCart ]
+                     return stateOfCart.map(item => item._id === action.payload._id ? { ...item, qt: item.qt+1 }: item )
             case "REMOVE_FROM_CART":
                 return stateOfCart.filter(item => item._id !== action.payload._id)
+            case "INCREASE_QUANTITY":
+                return stateOfCart.map(item => item._id === action.payload._id ? { ...item, qt: item.qt+1 }: item )
+            case "DECREASE_QUANTITY":
+                return stateOfCart.map(item => item._id === action.payload._id && item.qt >1 ? { ...item, qt: item.qt-1 }: item )
         }
     }
 
