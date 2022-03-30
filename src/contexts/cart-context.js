13525f13614fc7547f8284912b5b78a8d42d49ba
Namrata-J <namratajain29001@gmail.com>
2022-03-30 18:55:51 +0530

    fix: in the functionality

--- src/contexts/cart-context.js
+++ src/contexts/cart-context.js
@@ -7,11 +7,16 @@ const CartProvider = ({ children }) => {
 
     const cartReducer = (stateOfCart, action) => {
         switch(action.type){
+            case "MOVE_TO_CART":
+                if( !(stateOfCart.some(item => item._id === action.payload._id)) ){
+                    return [ ...stateOfCart, { ...action.payload, qt: 1 } ]
+                }else
+                    return stateOfCart.map(item => item._id === action.payload._id ? { ...item, qt: item.qt+1 }: item )
             case "ADD_TO_CART":
                 if( !(stateOfCart.some(item => item._id === action.payload._id)) ){
-                     return [ ...stateOfCart, { ...action.payload, qt: 1 } ]
+                    return [ ...stateOfCart, { ...action.payload, qt: 1 } ]
                 }else
-                     return stateOfCart.map(item => item._id === action.payload._id ? { ...item, qt: item.qt+1 }: item )
+                    return stateOfCart
             case "REMOVE_FROM_CART":
                 return stateOfCart.filter(item => item._id !== action.payload._id)
             case "INCREASE_QUANTITY":
