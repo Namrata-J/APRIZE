7a31575fb2aa5fa4d2a37abfba5bab1d3f5d1d02
Namrata-J <namratajain29001@gmail.com>
2022-04-14 18:43:48 +0530

    added seperate file for react icons

---
+++ src/utils/icons.js
@@ -0,0 +1,8 @@
+
+import { HiOutlineSearch } from "react-icons/hi";
+import { BiUser } from "react-icons/bi";
+import { BsCart3 } from "react-icons/bs";
+import { VscHeart } from "react-icons/vsc";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+export { HiOutlineSearch, BiUser, BsCart3, VscHeart, GiHamburgerMenu };
